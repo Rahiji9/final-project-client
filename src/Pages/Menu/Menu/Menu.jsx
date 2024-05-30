c35224169ab6f55425fdf842dbce07bc399949b5
rahiji9 <aliahmedrahi007@gmail.com>
2024-05-30 20:38:30 +0600

    2nd mod 2nd commiit

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg"
-import Popularmenu from "../../Home/PopularMenu/Popularmenu";
 
 const Menu = () => {
   return (
@@ -10,15 +9,7 @@ const Menu = () => {
         <title>Bistro Boss - Menu</title>
       </Helmet>
       <Cover img={menuImg} title="Our Menu"></Cover>
-      <Popularmenu></Popularmenu>
-      <Cover img={menuImg} title="Our Menu"></Cover>
-      <Popularmenu></Popularmenu>
-      <Cover img={menuImg} title="Our Menu"></Cover>
-      <Popularmenu></Popularmenu>
-      <Cover img={menuImg} title="Our Menu"></Cover>
-      <Popularmenu></Popularmenu>
-      <Cover img={menuImg} title="Our Menu"></Cover>
-      <Popularmenu></Popularmenu>
+      
       
     </div>
   );
